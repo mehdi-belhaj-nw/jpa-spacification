5c818b8e2f3f1de79d748737c6c38e2a90a8e610
Mehdi Belhaj <mehdi.belhaj@nimbleways.com>
2023-12-31 18:19:23 +0100

    🎉 first commit

---
+++ src/test/java/com/example/jpaspecifications/JpaSpecificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaspecifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
