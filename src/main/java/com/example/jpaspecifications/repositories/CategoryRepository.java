81ed72cb41283ce0e95bb91480da1882dbdcfb28
Mehdi Belhaj <mehdi.belhaj@nimbleways.com>
2024-01-02 17:56:09 +0100

    ✨ add Product and Category repositories

---
+++ src/main/java/com/example/jpaspecifications/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.jpaspecifications.repositories;
+
+import com.example.jpaspecifications.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/example/jpaspecifications/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.jpaspecifications.repositories;
+
+import com.example.jpaspecifications.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    // Additional custom queries can be defined here
+
+}
